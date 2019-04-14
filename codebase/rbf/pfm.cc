416dff46e692fc3ab5f4f483c9764cec3825d31d
kgurung <kgurung@ucsc.edu>
2019-04-14 16:34:46 -0700

    modified:   pfm.cc

--- codebase/rbf/pfm.cc
+++ codebase/rbf/pfm.cc
@@ -3,6 +3,9 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#include <errno.h>
+#include <string.h>
+
 PagedFileManager* PagedFileManager::_pf_manager = 0;
 
 PagedFileManager* PagedFileManager::instance()
@@ -28,13 +31,18 @@ RC PagedFileManager::createFile(const string &fileName)
 {
     const char * c = fileName.c_str();
     FILE* fd = fopen (c, "r");
-    if(fd == NULL){
-        FILE* fd = fopen (c, "w");
+    if(fd != nullptr){
+        fprintf(stderr, "Error: Attempting to create a file with the same name\n");
         fclose(fd);
-        return 0;
+        return -1;
+    }
+    fd = fopen(c, "w");
+    if(fd == nullptr){
+        fprintf(stderr, "Error: %s", strerror(errno));
+        return -1;
     }
     fclose(fd);
-    return -1;
+    return 0;
 }
 
 
@@ -42,8 +50,9 @@ RC PagedFileManager::destroyFile(const string &fileName)
 {
     const char * c = fileName.c_str();
     FILE* fd = fopen (c, "r");
-    if(fd == NULL){
-        return 0;
+    if(fd == nullptr){
+        fprintf(stderr, "Attempting to destroy file that does not exist!\n");
+        return -1;
     }
     fclose(fd);
     remove(c);
@@ -53,15 +62,36 @@ RC PagedFileManager::destroyFile(const string &fileName)
 
 RC PagedFileManager::openFile(const string &fileName, FileHandle &fileHandle)
 {
+    if(fileHandle.file != nullptr){
+        fprintf(stderr, "This file handle already has an open file!\n");
+        return -1;
+    }
     const char * c = fileName.c_str();
-    fileHandle.file = fopen(c, "rw+");
+    FILE* check = fopen(c, "r");
+    if(check == nullptr){
+        // fprintf(stderr, "Error: %s", strerror(errno));
+        fprintf(stderr, "Attempting to open file that does not exist!\n");
+        return -1;
+    }
+    fclose(check);
+    if((fileHandle.file = fopen(c, "rw+")) == nullptr){
+        fprintf(stderr, "Error: %s", strerror(errno));
+        return -1;
+    }
     return 0;
 }
 
 
 RC PagedFileManager::closeFile(FileHandle &fileHandle)
 {
-    fclose(fileHandle.file);
+    if(fileHandle.file == nullptr){
+        fprintf(stderr, "This file handle has no open file\n");
+        return -1;
+    }
+    if(fclose(fileHandle.file) != 0){
+        fprintf(stderr, "Error: %s", strerror(errno));
+        return -1;
+    }
     return 0;
 }
 
@@ -72,6 +102,7 @@ FileHandle::FileHandle()
 	writePageCounter = 0;
 	appendPageCounter = 0;
     pageCounter = 0;
+    file = nullptr;
 }
 
 
