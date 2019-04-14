cd9da12a24b4abc2c73366a2d5eeca2d6a75c720
Kenji Mah <kemmah@ucsc.edu>
2019-04-14 14:43:31 -0700

    try record print

--- codebase/rbf/rbfm.cc
+++ codebase/rbf/rbfm.cc
@@ -1,5 +1,6 @@
 #include "rbfm.h"
-
+#include <iostream>
+#include <cstring>
 RecordBasedFileManager* RecordBasedFileManager::_rbf_manager = 0;
 
 RecordBasedFileManager* RecordBasedFileManager::instance()
@@ -19,23 +20,42 @@ RecordBasedFileManager::~RecordBasedFileManager()
 }
 
 RC RecordBasedFileManager::createFile(const string &fileName) {
+    const char * c = fileName.c_str();
+    FILE* fd = fopen (c, "r");
+    if(fd == NULL){
+        FILE* fd = fopen (c, "w");
+        fclose(fd);
+        return 0;
+    }
+    fclose(fd);
     return -1;
 }
 
 RC RecordBasedFileManager::destroyFile(const string &fileName) {
-    return -1;
+    const char * c = fileName.c_str();
+    FILE* fd = fopen (c, "r");
+    if(fd == NULL){
+        return 0;
+    }
+    fclose(fd);
+    remove(c);
+    return 0;
 }
 
 RC RecordBasedFileManager::openFile(const string &fileName, FileHandle &fileHandle) {
-    return -1;
+    const char * c = fileName.c_str();
+    fileHandle.file = fopen(c, "rw+");
+    return 0;
 }
 
 RC RecordBasedFileManager::closeFile(FileHandle &fileHandle) {
-    return -1;
+    fclose(fileHandle.file);
+    return 0;
 }
 
 RC RecordBasedFileManager::insertRecord(FileHandle &fileHandle, const vector<Attribute> &recordDescriptor, const void *data, RID &rid) {
-    return -1;
+    
+    return 0;
 }
 
 RC RecordBasedFileManager::readRecord(FileHandle &fileHandle, const vector<Attribute> &recordDescriptor, const RID &rid, void *data) {
@@ -43,5 +63,24 @@ RC RecordBasedFileManager::readRecord(FileHandle &fileHandle, const vector<Attri
 }
 
 RC RecordBasedFileManager::printRecord(const vector<Attribute> &recordDescriptor, const void *data) {
-    return -1;
+    // (e.g., age: 24  height: 6.1  salary: 9000
+    //        age: NULL  height: 7.5  salary: 7500)
+//    char* buffer = NULL;
+//    memcpy(buffer, data, sizeof(char));
+//    cout << "test success" << endl;
+    char * nullFieldsIndicator;
+    memcpy(nullFieldsIndicator, (const char *)data, sizeof(char));
+    int counter = 0;
+    bool nullBit;
+    for (unsigned int i = 0; i < recordDescriptor.size(); ++i){
+        nullBit = nullFieldsIndicator & (1 << (5));
+        if (!nullBit){
+            cout << recordDescriptor[i].name << " Attr Type: " << (AttrType)recordDescriptor[i].type << " Attr Len: " << recordDescriptor[i].length << endl;
+
+        }
+        else cout << recordDescriptor[i].name << ": " << "NULL  ";
+        counter ++;
+    }
+    cout << endl;
+    return 0;
 }
